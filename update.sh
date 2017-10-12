92ade6da417a4193761dc8c6f0b0676a2b462dc8
Maxim Elakhov <evmax@users.noreply.github.com>
2017-10-12 14:06:13 +0300

    Fix

--- update.sh
+++ update.sh
@@ -14,16 +14,36 @@ packagesUrl='http://apt.postgresql.org/pub/repos/apt/dists/jessie-pgdg/main/bina
 packages="$(echo "$packagesUrl" | sed -r 's/[^a-zA-Z.-]+/-/g')"
 curl -sSL "${packagesUrl}.bz2" | bunzip2 > "$packages"
 
+travisEnv=
 for version in "${versions[@]}"; do
 	IFS=- read pg_major postgis_major <<< "$version"
 	fullVersion="$(grep -m1 -A10 "^Package: postgresql-$pg_major-postgis-$postgis_major\$" "$packages" | grep -m1 '^Version: ' | cut -d' ' -f2)"
 	[ -z "$fullVersion" ] && { echo >&2 "Unable to find package for PostGIS $postgis_major on Postgres $pg_major"; exit 1; }
 	(
 		set -x
-		cp Dockerfile.template initdb-postgis.sh README.md "$version/"
+		cp Dockerfile.template initdb-postgis.sh update-postgis.sh README.md "$version/"
 		mv "$version/Dockerfile.template" "$version/Dockerfile"
 		sed -i 's/%%PG_MAJOR%%/'$pg_major'/g; s/%%POSTGIS_MAJOR%%/'$postgis_major'/g; s/%%POSTGIS_VERSION%%/'$fullVersion'/g' "$version/Dockerfile"
 	)
+
+	srcVersion="${fullVersion%%+*}"
+	srcSha256="$(curl -sSL "https://github.com/postgis/postgis/archive/$srcVersion.tar.gz" | sha256sum | awk '{ print $1 }')"
+	for variant in alpine; do
+		if [ ! -d "$version/$variant" ]; then
+			continue
+		fi
+		(
+			set -x
+			cp Dockerfile.alpine.template initdb-postgis.sh update-postgis.sh "$version/$variant/"
+			mv "$version/$variant/Dockerfile.alpine.template" "$version/$variant/Dockerfile"
+			sed -i 's/%%PG_MAJOR%%/'"$pg_major"'/g; s/%%POSTGIS_VERSION%%/'"$srcVersion"'/g; s/%%POSTGIS_SHA256%%/'"$srcSha256"'/g' "$version/$variant/Dockerfile"
+		)
+		travisEnv="\n  - VERSION=$version VARIANT=$variant$travisEnv"
+	done
+
+	travisEnv='\n  - VERSION='"$version$travisEnv"
 done
+travis="$(awk -v 'RS=\n\n' '$1 == "env:" { $0 = "env:'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
+echo "$travis" > .travis.yml
 
 rm "$packages"
